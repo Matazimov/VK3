d02d2930677f4104f6cf3a96e0877e09217cba12
ilkhom <ilkhomzhonmatazimov@gmail.com>
2025-12-03 15:13:02 +0500

    фикс добавления из рекомендованных

--- handlers/friends_add_from_recommended_section/get_id.py
+++ handlers/friends_add_from_recommended_section/get_id.py
@@ -1,4 +1,4 @@
-from config import token
+from config import token, v
 from rich import print
 from cls import cls
 import requests
@@ -10,13 +10,14 @@ print("[yellow]Получаю список пользователей...[/yellow
 
 def get_id():
     data = requests.get("https://api.vk.com/method/friends.getSuggestions", params={
-        "v": 5.131,
-        "count": 500,
+        "v": v,
+        "count": 100,
         "filter": "mutual",
         "access_token": token,
-        "fields": "friend_status"
+        "fields": "friend_status",
     }).json()
     if "response" not in data:
+        print(data)
         print("[red]Произошла ошибка[/red]")
         sys.exit()
     data_filter = list(filter(lambda x: x["friend_status"] == 0, data["response"]["items"]))
